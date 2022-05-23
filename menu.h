b731e15dd82d19983c8af2ba74e1f2f952695f8b
Konstantyn <konstantyn.wenek2@gmail.com>
2022-05-23 23:35:22 +0200

    refactored

--- menu.h
+++ menu.h
@@ -1,12 +1,16 @@
 #include <iostream>
 
+
 int menu(){
-    int menunumber=-1;
-    std::cout<<"MENU\nChose option from 0 to 5:\n"; //later i will write here options +
-    while(menunumber>5||menunumber<0){ //i need to change menunumber to string to exclude option 
-        std::cin>>menunumber;           //that someone is going to put a letter in cin
-        if(menunumber>5||menunumber<0)
+    int menuNumber=-1;
+    std::cout<<"MENU\nChose option from 0 to 5:\n"; 
+    while(menuNumber>5||menuNumber<0)        //later i will write here options +
+    {                                        //i need to change menunumber to string to exclude option 
+        std::cin>>menuNumber;                //that someone is going to put a letter in cin     
+        if(menuNumber>5||menuNumber<0)
             std::cout<<"Chose option from 0 to 5:\n";
+
+        
     }
     
 }
