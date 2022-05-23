ee969b7892839d57f80af9ed28062bfff0ba4f5e
Konstantyn <konstantyn.wenek2@gmail.com>
2022-05-23 17:02:36 +0200

    added name

--- character.h
+++ character.h
@@ -2,17 +2,19 @@
 class character //vreating character with stats
 {
 public:
+    std::string name;
     int health;
     int damage;
     int stamina;
     int knowledge;
     character(){ //when created give him this stats
+        std::cin>>name;
         health = 5;
         damage = 5;
         stamina = 10;
         knowledge = 1;
     };
-    ~character() //on death this will show up on screen
+    ~character() //on death this will show up on screen (now at the end of the program)
     {
         std::cout<<"U died";
     };
