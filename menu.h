feab11f425fe05ac874f6796a3556ed97a6699d4
Konstantyn <konstantyn.wenek2@gmail.com>
2022-05-24 21:24:37 +0200

    added text and switch case for now nothing is in there

--- menu.h
+++ menu.h
@@ -4,9 +4,31 @@
 int menu(){
     int menuNumber=-1;
     std::cout<<"MENU\nChose option from 0 to 5:\n"; 
-    while(menuNumber>5||menuNumber<0)        //later i will write here options +
-    {                                        //i need to change menunumber to string to exclude option 
-        std::cin>>menuNumber;                //that someone is going to put a letter in cin     
+    while(menuNumber>5||menuNumber<0) //i need to change menunumber to string to exclude option 
+    {                                   //that someone is going to put a letter in cin
+        std::cout<<"0: exit\n1: stats\n2: move forward\n3: shop\n4: killed monsters\n";
+        std::cin>>menuNumber;                     
+        switch (menuNumber)
+        {
+        case 0:
+            /* code */
+            break;
+        case 1:                 
+            /* code */
+            break;
+        case 2:
+            /* code */
+            break;
+        case 3:
+            /* code */
+            break;
+        case 4:
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
         if(menuNumber>5||menuNumber<0)
             std::cout<<"Chose option from 0 to 5:\n";
 
