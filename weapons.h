71a94b52261bce133f15c96d3beb8bfc229d8752
Konstantyn <konstantyn.wenek2@gmail.com>
2022-05-30 02:50:46 +0200

    added functions to print out stats, i will remove this all and try to do class for every weapon to clean up main file

--- weapons.h
+++ weapons.h
@@ -1,9 +1,29 @@
 #include <iostream>
-
+#include <vector>
 class weapons{
     public:
-        int id;
-        int range;
-        int damage;
+        int id=0;
+        int range=0;
+        int damage=0;
         std::string type;
+        void setId(){
+            std::cout<<id;
+        }
+        void printRange()
+        {
+            std::cout<<range;
+        }
+        void printDamage()
+        {
+            std::cout<<damage;
+        }
+        void printType()
+        {
+            std::cout<<type;
+        }
+        void printAll(){
+            std::cout<<"\n"<<range<<"\n";
+            std::cout<<damage<<"\n";
+            std::cout<<type;
+        }
 };
